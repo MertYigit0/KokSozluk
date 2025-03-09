e32caa5f0526eb2e7db633c483e3ef9cb9d133da
merty <mertyigit148@gmail.com>
2025-03-09 16:51:12 +0300

    Initialize Spring Boot project with Kotlin and Gradle setup
    Create initial project structure with Spring Boot, Kotlin, and Gradle configuration. Add application entry point, basic properties, and test placeholders. Include necessary dependencies, Gradle wrapper files, and build settings.

---
+++ src/test/kotlin/com/mertyigit0/koksozluk/KoksozlukApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mertyigit0.koksozluk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KoksozlukApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
