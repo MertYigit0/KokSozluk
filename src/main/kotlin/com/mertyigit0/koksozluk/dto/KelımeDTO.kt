340c246599d7cda3d083aae884f722805da49485
merty <mertyigit148@gmail.com>
2025-03-10 14:52:42 +0300

    Add CRUD functionality for "Kelime" entity
    Introduced DTO, entity, repository, service, and controller layers to manage "Kelime" operations. Configured database connection and added bulk insertion support for "Kelime" records. This lays the foundation for handling word-related data in the application.

---
+++ src/main/kotlin/com/mertyigit0/koksozluk/dto/KelımeDTO.kt
@@ -0,0 +1,10 @@
+package com.mertyigit0.koksozluk.dto
+
+
+
+data class KelimeDTO(
+    val kelime: String,
+    val anlam: String,
+    val ornek_cumle: String,
+    val alternatif_kelime: String?
+)
